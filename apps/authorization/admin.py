b9f0301f7e8683d5c5a39f03f44c27ffd3d4357b
Nikita <Nif-Naf94@yandex.ru>
2023-10-31 22:57:37 +0300

    Настройка файла для административной панели.

--- apps/authorization/admin.py
+++ apps/authorization/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.authorization.models import User
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.reqister(User, UserAdmin)
