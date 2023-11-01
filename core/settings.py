31de05273caf026cc8bb23dbad4c5861a700087d
Nif-Naf <tna@eis24.me>
2023-11-01 13:48:03 +0300

    Установка djangorestframework_simplejwt как дефолтной аутентификации.

--- core/settings.py
+++ core/settings.py
@@ -163,6 +163,9 @@ LOGGING = {
 
 # DRF settings.
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
