f8a182ecfa63164bdf50792429f2e114b9701b3e
Nif-Naf <tna@eis24.me>
2023-11-03 13:22:06 +0300

    Добавление **extra_fields аргумент в методы менеджера модели User. Для передачи остальных параметров. Таких, как имя, фамилия и тп.

--- apps/authorization/models/manager.py
+++ apps/authorization/models/manager.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager
 class UserManager(BaseUserManager):
     """Менеджер модели User."""
 
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, password=None, **extra_fields):
         """Создание пользователя."""
         if username is None:
             raise TypeError('Users must have a username.')
@@ -15,18 +15,19 @@ class UserManager(BaseUserManager):
         user = self.model(
             username=username,
             email=self.normalize_email(email),
+            **extra_fields,
         )
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, username, email, password, **extra_fields):
         """Создание администратора."""
         if password is None:
             raise TypeError('Superusers must have a password.')
 
-        user = self.create_user(username, email, password)
+        user = self.create_user(username, email, password, **extra_fields)
         user.is_superuser = True
         user.is_staff = True
         user.save()
