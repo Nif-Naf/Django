7e37565ffb1ac541597a341e5958c20b781aeea8
Nif-Naf <tna@eis24.me>
2023-11-03 13:02:40 +0300

    Создание API для аутентификации. Регистрация его путей.

--- apps/authorization/urls.py
+++ apps/authorization/urls.py
@@ -2,15 +2,24 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from apps.authorization.views.authorization import SignUpAPIView
+from apps.authorization.views.authentication import SignInView, UpdateSignInView
 
 authorization = [
     # Создание нового пользователя.
     path('sign_up', SignUpAPIView.as_view(), name='sign_up'),
 ]
 
+authentication = [
+    # Получения access_token, refresh_token.
+    path('sign_in', SignInView.as_view(), name='login'),
+    # Обновление access_token.
+    path('update_sign_in', UpdateSignInView.as_view(), name='update_sign_in'),
+]
+
 user_router = DefaultRouter()
 
 urlpatterns = [
     *authorization,
+    *authentication,
     *user_router.urls,
 ]
