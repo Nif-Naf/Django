3444bd1c8a0d2670f1b0735122c2421beb85d035
Nif-Naf <tna@eis24.me>
2023-11-03 12:55:36 +0300

    Регистрация в корневом urls нового приложения authorization. Рефакторинг файла. Добавление базового пути.

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import (
     path,
     include,
 )
 
-from .settings import DEBUG
-from .swagger import urlpatterns as docs
+from core.swagger import urlpatterns as docs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/storage/', include('apps.storage.urls')),
+    path(settings.API_BASE_URL + 'auth/', include('apps.authorization.urls'), name='authorization'),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += docs
